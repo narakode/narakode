e5feebb1e80332e653783ef02cf825b2e4544da4
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-11-24 04:17:39 +0700

    navbar section dynamic

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,15 +4,25 @@ export const collection = {
     'layanan': defineCollection({
         type: 'data',
         schema: {
+            order: z.number(),
             title: z.string(),
             icon: z.string(),
             description: z.string(),
+            simpleDescription: z.string(),
             thumbnail: z.string(),
             startFrom: z.number(),
-            items: z.object({
+            items: z.array(z.object({
                 icon: z.string(),
-                name: z.string()
-            })
+                name: z.string(),
+                link: z.string(),
+                description: z.string(),
+                startFrom: z.number(),
+            })),
+            featureDescription: z.string(),
+            features: z.array(z.object({
+                icon: z.string(),
+                name: z.string(),
+            })),
         }
     })
 }
