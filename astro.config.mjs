748b14fb6597e4176d372da54ed0ebdb4543e504
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-09-19 15:20:06 +0700

    instal tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import icon from 'astro-icon';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
