988036e9c08df9bfd02e590f5bf1ef236929afff
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-09-30 15:23:48 +0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  site: 'https://narakode.com',
+  integrations: [icon(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
