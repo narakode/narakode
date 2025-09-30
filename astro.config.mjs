d8b91a5263b16eb1fdc6c91f9d34eee793bdb1d7
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-09-30 21:13:57 +0700

    search

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
-import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://narakode.com',
-  integrations: [icon(), sitemap(), pagefind()],
+  integrations: [icon(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
