f488b6747b089cfc8156702a2796043a884749e8
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-10-01 15:44:49 +0700

    astro mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://narakode.com',
-  integrations: [icon(), sitemap()],
+  integrations: [icon(), sitemap(), mdx()],
 
   vite: {
     plugins: [tailwindcss()]
