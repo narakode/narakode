0c6d7fe31a9623d27ba56d222ac634adca08e9fe
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-10-01 14:23:15 +0700

    search data

---
+++ src/pages/search.json.ts
@@ -0,0 +1,23 @@
+import { getCollection } from "astro:content"
+
+export async function GET() {
+    const articles = await getCollection('article', article => article.data.publishedAt)
+    
+    return new Response(JSON.stringify(
+        articles.map(article => {
+            if (article.data.type === 'course') {
+                return {
+                    title: article.data.title,
+                    url: `/courses/${article.data.course?.id}/${article.id}`,
+                    description: article.data.description
+                }
+            }
+
+            return {
+                title: article.data.title,
+                url: article.id,
+                description: article.data.description
+            }
+        })
+    ))
+}
