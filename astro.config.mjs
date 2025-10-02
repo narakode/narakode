7f5a5a953e681a209c2f27e6adc8ad7cedafde98
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-10-02 19:51:04 +0700

    ga

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://narakode.id',
-  integrations: [icon(), sitemap(), mdx()],
+  integrations: [icon(), sitemap(), mdx(), partytown({ config: { forward: ['dataLayer.push'] } })],
 
   vite: {
     plugins: [tailwindcss()]
