41200e32d0e6e7ac112290fa5f5ad2f72cac35fb
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-10-02 13:22:47 +0700

    course filter published items

--- src/utils/course.ts
+++ src/utils/course.ts
@@ -1,9 +1,12 @@
 import { getCollection, type CollectionEntry } from "astro:content";
 
 export async function getCourseItems(course: CollectionEntry<'course'>): Promise<CollectionEntry<'article'>[]> {
-    const items = await getCollection('article', article => article.data.course?.id === course.id && !!article.data.publishedAt)
+    const items = await getCollection('article', article => article.data.course?.id === course.id)
 
     items.sort((a, b) => new Date(a.data.publishedAt!).getTime() - new Date(b.data.publishedAt!).getTime())
 
     return items
+}
+export function filterPublishedItems(entries: CollectionEntry<'article'>[]): CollectionEntry<'article'>[] {
+    return entries.filter(entry => new Date > new Date(entry.data.publishedAt!))
 }
