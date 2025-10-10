50ea0dd923beef2e55dfa9ceb9e38723da16dc11
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-10-10 10:59:40 +0700

    get course util

--- src/utils/course.ts
+++ src/utils/course.ts
@@ -10,3 +10,11 @@ export async function getCourseItems(course: CollectionEntry<'course'>): Promise
 export function filterPublishedItems(entries: CollectionEntry<'article'>[]): CollectionEntry<'article'>[] {
     return entries.filter(entry => new Date > new Date(entry.data.publishedAt!))
 }
+
+export async function getCourses(): Promise<CollectionEntry<'course'>[]> {
+    const courses = await getCollection('course', course => course.data.publishedAt)
+
+    courses.sort((a, b) => new Date(a.data.publishedAt!).getTime() - new Date(b.data.publishedAt!).getTime())
+
+    return courses
+}
