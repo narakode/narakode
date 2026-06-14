888e82d325ecf0ef1788a825833dfb960e695a4d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-06-14 17:20:29 +0700

    v-todo

---
+++ src/site.ts
@@ -0,0 +1,5 @@
+export const site = {
+  title: 'Narakode - Penyedia layanan dan produk digital untuk bisnis',
+  description: 'Solusi untuk bisnis melalui layanan dan produk digital',
+  image: 'https://narakode.id/banner.png'
+}
